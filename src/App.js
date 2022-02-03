3312e985295c6d9f8a9644c6f5c8662c7379f29f
Yash Yadav <yashyadavgit@gmail.com>
2022-02-04 01:29:08 +0530

    Random Basic Generator Finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+
+  useEffect(() => {
+    getQuotes();
+  }, []);
+
+  function getQuotes() {
+    fetch(`https://type.fit/api/quotes`)
+      .then((respons) => respons.json())
+      .then((data) => {
+        let randomNumber = Math.floor(Math.random() * data.length);
+        let randomQuote = data[randomNumber].text;
+        setAdvice(randomQuote);
+      });
+  }
+  console.log(advice);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {getQuotes === undefined ? <h1>Hello</h1> : <h1>{advice}</h1>}
+      <button onClick={getQuotes}>Next Quote</button>
     </div>
   );
 }
